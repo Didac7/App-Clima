e5ea98de30dd6ae10cfa97cb331a517c0500f792
Didac <ediego918@gmail.com>
2024-12-28 10:34:40 -0400

    app terminada

--- app-clima/src/pages/AppClima.jsx
+++ app-clima/src/pages/AppClima.jsx
@@ -1,5 +1,5 @@
 import { Container, Typography } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useWeatherApi } from "../hooks/useWeatherApi";
 import { Form } from "../components/Form";
 import { WeatherInfo } from "../components/WeatherInfo";
@@ -8,13 +8,37 @@ export const AppClima = () => {
     const [city, setCity] = useState("");
     const { clima, loading, error, fetchWeather } = useWeatherApi();
 
+    const [background, setBackground] = useState(''); // Para almacenar el fondo dinámico
+
+    useEffect(() => {
+        if (clima.conditionText) {
+            switch (clima.conditionText.toLowerCase()) {
+                case 'soleado':
+                    setBackground('url(https://png.pngtree.com/background/20230519/original/pngtree-cartoon-sun-in-a-sunny-landscape-picture-image_2666701.jpg)'); // Ruta de la imagen de fondo soleado
+                    break;
+                case 'parcialmente nublado':
+                    setBackground('url(https://aforisticamente.com/wp-content/uploads/2018/03/nubes3a.jpg)'); // Ruta de la imagen de fondo nublado
+                    break;
+                case 'lluvia  moderada a intervalos':
+                    setBackground('url(https://thumbs.dreamstime.com/b/paisaje-de-dibujos-animados-con-pozo-piedra-y-lluvia-vector-viejo-agua-potable-en-la-colina-verde-el-tiempo-lluvioso-verano-214182372.jpg)'); // Ruta de la imagen de fondo lluvioso
+                    break;
+                default:
+                    setBackground(''); // Fondo por defecto
+                    break;
+            }
+        }
+    }, [clima.conditionText]); // Se ejecuta cada vez que cambie la condición climática
+
     const onSubmit = (evento) => {
         evento.preventDefault();
         fetchWeather(city);
     };
 
     return (
-        <Container maxWidth="xs" sx={{ mt: 2 }}>
+        <Container 
+            maxWidth="xs" 
+            sx={{ mt: 2, backgroundImage: background, backgroundSize: 'cover', minHeight: '100vh' }}
+        >
             <Typography variant="h3" component="h1" align="center" gutterBottom>
                 Clima App
             </Typography>
